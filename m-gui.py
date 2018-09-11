8d6b53d08e79d315512feb21b8a187ff970e3d34
Felix C. Stegerman <flx@obfusk.net>
2018-09-12 01:07:52 +0200

    improvements incl. scrolling, fullscreen, hotkeys, --scale

--- m-gui.py
+++ m-gui.py
@@ -5,7 +5,7 @@
 #
 # File        : m-gui.py
 # Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-# Date        : 2018-09-10
+# Date        : 2018-09-11
 #
 # Copyright   : Copyright (C) 2018  Felix C. Stegerman
 # Version     : v0.0.1
@@ -21,25 +21,29 @@ r"""
 
 # === TODO ===
 #
-# * cover most important commands
-# * control w/ cursor+enter?
-#   - terminal vs list view?
-# * checkboxes etc. for --options
-# * menu icons?
+# * cover most important commands!
+#   - la, nn, p, m, u, s, i, alias, ...
+# * allow choices (input / choose) for arguments for mark etc.
+# * checkboxes etc. for --options, --options -> m, config.json?!
+#   - --show-hidden --ignorecase --numeric-sort
+#   - --numbers
+#   - | column
+# * use shell "m ..." if no need to quote?
+# * README etc., document, test?, package
+# * (MAYBE) about dialog, help etc.
+# * (MAYBE) menu icons?
+# * (MAYBE) control w/ cursor+enter? // terminal vs list view?
+
+# Depends: python3:any (>= 3.5~), python3-gi, libgtk-3-0, libvte-2.91-0
 
 # === imports ===
 
-import gi, os, sys
+import argparse, gi, os, sys, time
 import xml.etree.ElementTree as ET
 
 from pathlib import Path
 
-os.environ["GDK_DPI_SCALE"] = "1.7"                             # TODO
-
-gi.require_version("Gtk", "3.0")
-gi.require_version("Vte", "2.91")
-
-from gi.repository import GLib, Gio, Gtk, Pango, Vte
+# see also import_gtk()
 
 # === vars ===
 
@@ -48,7 +52,10 @@ __version__ = "0.0.1"
 TITLE       = "m - minimalistic media manager - GUI"
 APPID       = "ch.obfusk.m.gui"
 SIZE        = (1280, 720)
+
 MCMD        = "m"
+SCALE       = 1.5
+SCROLLBACK  = 1024
 
 # NB: we run a login shell b/c we need /etc/profile.d/vte-2.91.sh to
 # be sourced for current_directory_uri to work.
@@ -56,180 +63,232 @@ SHELL       = "/bin/bash --login".split()
 
 # === classes ===
 
-# TODO
-class Term(Vte.Terminal):                                       # {{{1
-  """Terminal for m."""
+def define_classes():
+  global Term, AppWin, App
 
-  FLG = GLib.SpawnFlags.DO_NOT_REAP_CHILD | GLib.SpawnFlags.SEARCH_PATH
+  class Term(Vte.Terminal):                                     # {{{1
+    """Terminal for m."""
 
-  def __init__(self, spawned_callback = None, exited_callback = None,
-               chdir_callback = None, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.connect("child-exited", self.on_child_exited)
-    self.connect("current-directory-uri-changed", self.on_cdu_changed)
-    self.spawned_callback = spawned_callback
-    self.exited_callback  = exited_callback
-    self.chdir_callback   = chdir_callback
+    FLG = GLib.SpawnFlags.DO_NOT_REAP_CHILD | GLib.SpawnFlags.SEARCH_PATH
 
-  def run(self, *cmd, **kwargs):
-    """Run command in terminal."""
-    info("*** RUN ***", cmd)
-    if kwargs.get("reset", True): self.reset(False, True)
-    _, pid = self.spawn_sync(Vte.PtyFlags.DEFAULT, None, cmd, [],
-                             self.FLG, None, None)
-    if self.spawned_callback: self.spawned_callback(pid)
+    def __init__(self, spawned_callback = None, exited_callback = None,
+                 chdir_callback = None, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.set_scrollback_lines(SCROLLBACK)
+      self.connect("child-exited", self.on_child_exited)
+      self.connect("current-directory-uri-changed", self.on_cdu_changed)
+      self.spawned_callback = spawned_callback
+      self.exited_callback  = exited_callback
+      self.chdir_callback   = chdir_callback
 
-  # TODO: only for testing
-  def say(self, msg):
-    self.run("echo", msg)
-
-  def sh(self):
-    self.run(*SHELL)
-
-  def on_child_exited(self, _term, status):
-    if self.exited_callback: self.exited_callback(status)
-
-  def on_cdu_changed(self, _term):
-    uri = self.props.current_directory_uri
-    if uri:
-      d = GLib.filename_from_uri(uri)[0]
-      if self.chdir_callback: self.chdir_callback(d)
-                                                                # }}}1
-
-class AppWin(Gtk.ApplicationWindow):                            # {{{1
-  """Main application window."""
-
-  def __init__(self, term_args = {}, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.set_default_size(*SIZE)
-    self.cwd_lbl, self.term = Gtk.Label(cwd()), Term(**term_args)
-    self.cwd_lbl.set_ellipsize(Pango.EllipsizeMode.MIDDLE)
-    box = Gtk.Box(orientation = Gtk.Orientation.VERTICAL)
-    box.pack_start(self.cwd_lbl, False, True, 0)
-    box.pack_start(self.term   , True , True, 0)
-    self.add(box)
-                                                                # }}}1
-
-# TODO
-class App(Gtk.Application):                                     # {{{1
-  """Main application."""
-
-  # TODO: flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE ?
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, application_id = APPID, **kwargs)
-    self.win, self.actions, self.fullscreen = None, [], False
-
-  def do_startup(self):                                         # {{{2
-    Gtk.Application.do_startup(self)
-    builder = Gtk.Builder.new_from_string(MENU_XML, -1)
-    self.set_menubar(builder.get_object("menubar"))
-    for name in ACTIONS:
-      cb = "on_{}".format(name)
-      if hasattr(self, cb):
-        self.add_simple_action(name, getattr(self, cb))
-      else:
-        self.add_simple_action(name, self.on_something)
+    def run(self, *cmd, **kwargs):                              # {{{2
+      """Run command in terminal."""
+      info("*** RUN ***", cmd)
+      if kwargs.get("reset", True): self.reset(False, True)
+      header = kwargs.get("header")
+      if header: self.feed(header.replace("\n", "\n\r").encode())
+      _, pid = self.spawn_sync(Vte.PtyFlags.DEFAULT, None, cmd, [],
+                               self.FLG, None, None)
+      if self.spawned_callback: self.spawned_callback(pid)
+      time.sleep(0.1)   # seems to help
                                                                 # }}}2
 
-  def add_simple_action(self, name, callback):
-    action = Gio.SimpleAction.new(name, None)
-    action.connect("activate", callback)
-    self.add_action(action)
-    self.actions.append(action)
+    def sh(self):
+      self.run(*SHELL)
+
+    def on_child_exited(self, _term, status):
+      if self.exited_callback: self.exited_callback(status)
+
+    # NB: this only works if /etc/profile.d/vte-2.91.sh is sourced
+    def on_cdu_changed(self, _term):
+      uri = self.props.current_directory_uri
+      if uri:
+        d = GLib.filename_from_uri(uri)[0]
+        if self.chdir_callback: self.chdir_callback(d)
+                                                                # }}}1
+
+  class AppWin(Gtk.ApplicationWindow):                          # {{{1
+    """Main application window."""
+
+    def __init__(self, term_args = {}, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.set_default_size(*SIZE)
+      self.cwd_lbl, self.term = Gtk.Label(cwd()), Term(**term_args)
+      self.cwd_lbl.set_ellipsize(Pango.EllipsizeMode.MIDDLE)
+      box = Gtk.Box(orientation = Gtk.Orientation.VERTICAL)
+      box.pack_start(self.cwd_lbl, False, True, 0)
+      box.pack_start(self.term   , True , True, 0)
+      self.add(box)
+                                                                # }}}1
 
   # TODO
-  def do_activate(self):                                        # {{{2
-    if not self.win:
-      term_args = dict(spawned_callback = self.on_cmd_spawned,
-                       exited_callback  = self.on_cmd_exited,
-                       chdir_callback   = self.on_chdir)
-      self.win  = AppWin(application = self, title = TITLE,
-                         term_args = term_args)
-    self.win.show_all()
-    self.win.present()
-    self.win.fullscreen()                                       # TODO
+  class App(Gtk.Application):                                   # {{{1
+    """Main application."""
+
+    # TODO: flags=Gio.ApplicationFlags.HANDLES_COMMAND_LINE ?
+    def __init__(self, *args, fullscreen = False,
+                 stay_fullscreen = False, **kwargs):
+      super().__init__(*args, application_id = APPID, **kwargs)
+      self.win, self.actions, self.noquit = None, [], False
+      self.is_fs, self.stay_fs, self.start_fs \
+        = False, stay_fullscreen, fullscreen
+
+    def do_startup(self):
+      Gtk.Application.do_startup(self)
+      builder = Gtk.Builder.new_from_string(MENU_XML, -1)
+      self.set_menubar(builder.get_object("menubar"))
+      for name in ACTIONS:
+        cb = "on_{}".format(name)
+        self.add_simple_action(name, getattr(self, cb))
+
+    def add_simple_action(self, name, callback):
+      action = Gio.SimpleAction.new(name, None)
+      action.connect("activate", callback)
+      self.add_action(action)
+      self.actions.append(action)
+
+    def do_activate(self):                                      # {{{2
+      if not self.win:
+        term_args = dict(spawned_callback = self.on_cmd_spawned,
+                         exited_callback  = self.on_cmd_exited,
+                         chdir_callback   = self.on_chdir)
+        self.win  = AppWin(application = self, title = TITLE,
+                           term_args = term_args)
+        self.win.connect("window-state-event", self.on_window_state_event)
+      self.win.show_all()
+      self.win.present()
+      if self.start_fs: self.win.fullscreen()
                                                                 # }}}2
 
-  def on_opendir(self, _action, _param):
-    d = self.choose_folder()
-    if d is not None:
-      os.chdir(d)
-      self.on_chdir(d)
+    def on_opendir(self, _action, _param):
+      d = self.choose_folder()
+      if d is not None:
+        os.chdir(d)
+        self.on_chdir(d)
 
-  def on_dirup(self, _action, _param):
-    chdir_up()
-    self.on_chdir(cwd())
+    def on_dirup(self, _action, _param):
+      chdir_up()
+      self.on_chdir(cwd())
 
-  def on_shell(self, _action, _param):
-    self.win.term.sh()
+    def on_shell(self, _action, _param):
+      self.noquit = True
+      self.win.term.sh()
 
-  def on_quit(self, _action, _param):
-    self.quit()
+    def on_quit(self, _action, _param):
+      self.quit()
 
-  def on_list(self, _action, _param):
-    self.run_m("ls")
+    def on_list(self, _action, _param):
+      self.run_m("ls")
 
-  def on_listdirs(self, _action, _param):
-    self.run_m("ld")
+    def on_listdirs(self, _action, _param):
+      self.run_m("ld")
 
-  def on_next(self, _action, _param):
-    self.fullscreen = True
-    self.run_m("next")
+    def on_next(self, _action, _param):
+      self.run_m("next")
 
-  # ... TODO ...
+    # ... TODO ...
 
-  # TODO: only for testing
-  def on_something(self, action, _param):
-    msg = ">>> {} <<<".format(action.get_name())
-    self.win.term.say(msg)
+    def on_pgup(self, _action, _param):
+      v = self._vadj; v.set_value(v.get_value() - v.get_page_increment())
 
-  def on_cmd_spawned(self, pid):
-    info("*** SPAWN ***", "pid =", pid)
-    for action in self.actions:
-      if action.get_name() != "quit":
-        action.set_enabled(False)
+    def on_pgdn(self, _action, _param):
+      v = self._vadj; v.set_value(v.get_value() + v.get_page_increment())
 
-  def on_cmd_exited(self, status):
-    info("*** EXIT ***", "status =", status)
-    for action in self.actions:
-      action.set_enabled(True)
-    if self.fullscreen:
-      self.fullscreen = False
-      self.win.fullscreen()                                     # TODO
+    def on_top(self, _action, _param):
+      v = self._vadj; v.set_value(v.get_lower())
 
-  def on_chdir(self, d):
-    info("*** CHDIR ***", d)
-    self.win.cwd_lbl.set_text(d)
+    def on_bottom(self, _action, _param):
+      v = self._vadj; v.set_value(v.get_upper())
 
-  def choose_folder(self):                                      # {{{2
-    dialog = Gtk.FileChooserDialog(
-      "Please choose a folder", self.win,
-      Gtk.FileChooserAction.SELECT_FOLDER,
-      (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-       Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
-    )
-    dialog.set_filename(cwd())                                  # TODO
-    try:
-      response = dialog.run()
-      if response == Gtk.ResponseType.OK:
-        return dialog.get_filename()
+    @property
+    def _vadj(self):
+      return self.win.term.props.vadjustment
+
+    def on_fullscreen(self, _action, _param):
+      if self.is_fs:
+        self.win.unfullscreen()
       else:
-        return None
-    finally:
-      dialog.destroy()
+        self.win.fullscreen()
+
+    def on_cmd_spawned(self, pid):
+      info("*** SPAWN ***", "pid =", pid)
+      for action in self.actions:
+        if self.noquit or action.get_name() != "quit":
+          action.set_enabled(False)
+      self.noquit = False
+
+    def on_cmd_exited(self, status):
+      info("*** EXIT ***", "status =", status)
+      for action in self.actions:
+        action.set_enabled(True)
+      if self.stay_fs: self.win.fullscreen()
+
+    def on_chdir(self, d):
+      info("*** CHDIR ***", d)
+      self.win.cwd_lbl.set_text(d)
+
+    def on_window_state_event(self, widget, event):
+      self.is_fs = bool(event.new_window_state &
+                        Gdk.WindowState.FULLSCREEN)
+
+    def choose_folder(self):                                    # {{{2
+      dialog = Gtk.FileChooserDialog(
+        "Please choose a folder", self.win,
+        Gtk.FileChooserAction.SELECT_FOLDER,
+        (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+         Gtk.STOCK_OPEN, Gtk.ResponseType.OK)
+      )
+      dialog.set_filename(cwd())                                # TODO
+      try:
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+          return dialog.get_filename()
+        else:
+          return None
+      finally:
+        dialog.destroy()
                                                                 # }}}2
 
-  # TODO: what about opts & args? ask or allow choosing?
-  def run_m(self, *cmd):
-    fcmd              = (MCMD,) + cmd
-    os.environ["PWD"] = cwd()
-    self.win.term.feed("$ {}\r\n".format(" ".join(fcmd)).encode())
-    self.win.term.run(*fcmd)
+    # TODO: what about opts & args? ask or allow choosing?
+    def run_m(self, *cmd):
+      os.environ["PWD"] = cwd()
+      fcmd              = (MCMD,) + cmd
+      header            = "$ {}\n".format(" ".join(fcmd))
+      self.win.term.run(*fcmd, header = header)
                                                                 # }}}1
 
 # === functions ===
 
-def main(*args): App().run()
+def import_gtk(scale = SCALE):
+  global GLib, Gio, Gdk, Gtk, Pango, Vte
+  os.environ["GDK_DPI_SCALE"] = str(scale)
+  gi.require_version("Gtk", "3.0")
+  gi.require_version("Gdk", "3.0")
+  gi.require_version("Vte", "2.91")
+  from gi.repository import GLib, Gio, Gdk, Gtk, Pango, Vte
+
+def main(*args):
+  n = _argument_parser().parse_args(args)
+  import_gtk(scale = n.scale)
+  define_classes()
+  App(fullscreen = n.fullscreen or n.stay_fullscreen,
+      stay_fullscreen = n.stay_fullscreen).run()
+
+def _argument_parser():                                         # {{{1
+  p = argparse.ArgumentParser(description = TITLE)
+  p.set_defaults(scale = SCALE, fullscreen = False,
+                 stay_fullscreen = False)
+  p.add_argument("--version", action = "version",
+                 version = "%(prog)s {}".format(__version__))
+  p.add_argument("--scale", "-s", metavar = "SCALE", type = float,
+                 help = "set $GDK_DPI_SCALE to SCALE")
+  p.add_argument("--fullscreen", "--fs", action = "store_true",
+                 help = "start full screen")
+  p.add_argument("--stay-fullscreen", "--stay-fs",
+                 action = "store_true",
+                 help   = "start and stay full screen")
+  return p
+                                                                # }}}1
 
 def info(*msgs): print(*msgs, file = sys.stderr)
 
@@ -251,25 +310,26 @@ MENU_XML = """
         <item>
           <attribute name="action">app.opendir</attribute>
           <attribute name="label" translatable="yes">_Open Directory...</attribute>
-          <attribute name="accel">&lt;Primary&gt;o</attribute>
+          <attribute name="accel">o</attribute>
         </item>
         <item>
           <attribute name="action">app.dirup</attribute>
-          <attribute name="label" translatable="yes">Go to _Parent Directory...</attribute>
-          <attribute name="accel">&lt;Primary&gt;u</attribute>
+          <attribute name="label" translatable="yes">Open _Parent Directory (..)</attribute>
+          <attribute name="accel">period</attribute>
         </item>
       </section>
       <section>
         <item>
           <attribute name="action">app.shell</attribute>
-          <attribute name="label" translatable="yes">Open _Shell</attribute>
+          <attribute name="label" translatable="yes">Run _Shell</attribute>
+          <attribute name="accel">dollar</attribute>
         </item>
       </section>
       <section>
         <item>
           <attribute name="action">app.quit</attribute>
           <attribute name="label" translatable="yes">_Quit</attribute>
-          <attribute name="accel">&lt;Primary&gt;q</attribute>
+          <attribute name="accel">q</attribute>
         </item>
       </section>
     </submenu>
@@ -279,18 +339,53 @@ MENU_XML = """
         <item>
           <attribute name="action">app.list</attribute>
           <attribute name="label" translatable="yes">_List</attribute>
-          <attribute name="accel">&lt;Primary&gt;l</attribute>
+          <attribute name="accel">l</attribute>
         </item>
         <item>
           <attribute name="action">app.listdirs</attribute>
           <attribute name="label" translatable="yes">List _Directories</attribute>
+          <attribute name="accel">d</attribute>
         </item>
       </section>
       <section>
         <item>
           <attribute name="action">app.next</attribute>
           <attribute name="label" translatable="yes">Play _Next</attribute>
-          <attribute name="accel">&lt;Primary&gt;n</attribute>
+          <attribute name="accel">n</attribute>
+        </item>
+      </section>
+    </submenu>
+    <submenu>
+      <attribute name="label" translatable="yes">_Window</attribute>
+      <section>
+        <item>
+          <attribute name="action">app.pgup</attribute>
+          <attribute name="label" translatable="yes">Scroll _Up</attribute>
+          <attribute name="accel">Page_Up</attribute>
+        </item>
+        <item>
+          <attribute name="action">app.pgdn</attribute>
+          <attribute name="label" translatable="yes">Scroll _Down</attribute>
+          <attribute name="accel">Page_Down</attribute>
+        </item>
+      </section>
+      <section>
+        <item>
+          <attribute name="action">app.top</attribute>
+          <attribute name="label" translatable="yes">Scroll to _Top</attribute>
+          <attribute name="accel">Home</attribute>
+        </item>
+        <item>
+          <attribute name="action">app.bottom</attribute>
+          <attribute name="label" translatable="yes">Scroll to _Bottom</attribute>
+          <attribute name="accel">End</attribute>
+        </item>
+      </section>
+      <section>
+        <item>
+          <attribute name="action">app.fullscreen</attribute>
+          <attribute name="label" translatable="yes">Toggle _Fullscreen</attribute>
+          <attribute name="accel">f</attribute>
         </item>
       </section>
     </submenu>
